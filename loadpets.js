7e0dab81985e55d2a0e9e77be9be1e38721629a4
HarryLynnOo <harrylynnoo904@gmail.com>
2025-06-18 15:14:50 +0700

    finalized

--- loadpets.js
+++ loadpets.js
@@ -1,19 +1,14 @@
 const pets = [
-    {
-        "name": "Buddy",
-        "type": "Dog",
-        "age": 3,
-        "img": "img/dogs/dog01.jpg"
-    },
-    {
-        "name": "Buddy",
-        "type": "Dog",
-        "age": 3,
-        "img": "img/dogs/dog02.jpg"
-    },
-    { "name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg" },
-    { "name": "Mittens", "type": "Cat", "age": 2, "img": "img/cats/cat02.jpg" },
-
+    {"name": "Buddy","type": "Dog","age": 3,"img": "img/dogs/dog01.jpg"},
+    {"name": "Teddy","type": "Dog","age": 3,"img": "img/dogs/dog02.jpg"},
+    {"name": "Moony", "type": "Dog", "age": 2, "img": "img/dogs/dog03.jpg" },
+    {"name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg" },
+    {"name": "Mittens", "type": "Cat", "age": 2, "img": "img/cats/cat02.jpg" },
+    {"name": "Sunny", "type": "Cat", "age": 2, "img": "img/cats/cat03.jpg" },
+    {"name": "Jade", "type": "Bird", "age": 1, "img": "img/birds/bird01.jpg" },
+    {"name": "Azure", "type": "Bird", "age": 1, "img": "img/birds/bird02.jpg" },
+    {"name": "Freddy", "type": "Capybara", "age": 2, "img": "img/capybaras/capybara01.jpg" },
+    {"name": "Georgie", "type": "Capybara", "age": 2, "img": "img/capybaras/capybara02.jpg" },
 ]
 var ap = document.getElementById("all-pets")
 // ap.innerHTML = "<h1>Hello World</h1>";
